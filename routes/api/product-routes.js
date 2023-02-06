c6a280f1c8a3f3eec1b45ab3f631ad16270c82e6
BRosencrans <brandon.rosencrans@gmail.com>
2023-02-05 18:51:50 -0800

    added post route for new products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -58,19 +58,18 @@ router.get('/:id', (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagId: req.body.tag_id
+  })
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+      if (req.body.tagId.length) {
+        const productTagIdArr = req.body.tagId.map((tag_id) => {
           return {
             product_id: product.id,
             tag_id,
